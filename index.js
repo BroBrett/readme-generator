71f57f98cfb21e1a5ff914d2543082571c4b5ad0
brett hockridge <bretthockridge98@gmail.com>
2022-05-07 13:05:51 -0700

    created an array of questions for user input

--- index.js
+++ index.js
@@ -3,7 +3,54 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Give a brief description of your project.'
+    },
+    {
+        type: 'input',
+        name: 'Installation',
+        message: 'What are the instructions for the installation of this project?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use your project?'
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Select a license for your project.',
+        choices: ['MIT', 'Apache', 'BSD', 'GNU']
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'How can others provide contribution to your project?'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What are the test instructions?'
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your Github username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email adress?'
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
