e776d79329753490a4c5c0a88b3a6471dfce5518
brett hockridge <bretthockridge98@gmail.com>
2022-05-16 20:49:05 -0700

    fixed small issue on index.js

--- index.js
+++ index.js
@@ -5,55 +5,65 @@ const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [
     {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your project?'
-    },
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    }, 
     {
-        type: 'input',
-        name: 'description',
-        message: 'Give a brief description of your project.'
-    },
+        type: "input",
+        name: "description",
+        message: "What is the description of your project?"
+    }, 
     {
-        type: 'input',
-        name: 'Installation',
-        message: 'What are the instructions for the installation of this project?'
-    },
+        type: "input",
+        name: "installation",
+        message: "What are the instructions for installation?"
+    }, 
     {
-        type: 'input',
-        name: 'usage',
-        message: 'How do you use your project?'
-    },
+        type: "input",
+        name: "usage",
+        message: "What are the instructions for usage for your project?"
+    }, 
     {
-        type: 'checkbox',
-        name: 'license',
-        message: 'Select a license for your project.',
-        choices: ['MIT', 'Apache', 'BSD', 'GNU']
-    },
+        type: "checkbox",
+        name: "license",
+        message: "Select A License.",
+        choices: ["Apache", "MIT", "GNU", "BSD"]
+    }, 
     {
-        type: 'input',
-        name: 'contributors',
-        message: 'How can others provide contribution to your project?'
-    },
+        type: "input",
+        name: "contributions",
+        message: "Enter instructions on how to contribute to your project."
+    }, 
     {
-        type: 'input',
-        name: 'tests',
-        message: 'What are the test instructions?'
-    },
+        type: "input",
+        name: "tests",
+        message: "Enter tests information."
+    }, 
     {
-        type: 'input',
-        name: 'username',
-        message: 'What is your Github username?'
-    },
+        type: "input",
+        name: "credits",
+        message: "Would you like to give any credit(s) to other individuals?"
+    }, 
     {
-        type: 'input',
-        name: 'email',
-        message: 'What is your email adress?'
-    },
+        type: "input",
+        name: "author",
+        message: "Name(s) of the author(s) of this project."
+    }, 
+    {
+        type: "input",
+        name: "username",
+        message: "What is your github username?"
+    }, 
+    {
+        type: "input",
+        name: "email",
+        message: "What is the email you would like to use?"
+    }
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
+// This is the function that writes the README file
+function writeInFile(fileName, data) {
     fs.writeFile(fileName + '.md', data, function (error) {
         if (error) {
             console.log(error)
@@ -61,10 +71,10 @@ function writeToFile(fileName, data) {
     })
 };
 
-// TODO: Create a function to initialize app
+// This is the asynchronous function to initialize the application
 async function init() {
-    const data = await inquirer.prompt(questions);
-    writeToFile('README', generateMarkdown(data));
+    const info = await inquirer.prompt(questions);
+    writeInFile("README", generateMarkdown(info));
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,4 +1,4 @@
-let licenseBadge = '';
+let licenseBadge = "";
 
 function licenseRender(data) {
   for (const license of data.license) {
@@ -16,48 +16,66 @@ function licenseRender(data) {
 }
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-  ${licenseRender(data)}
-
-  ## Description
-  ${data.descritpion}
-
-
-  ## Table of Contents:
-  * [Installation](#installation)
-  * [Usage](#usage)
-  * [License](#license)
-  * [Contributions](#contributions)
-  * [Tests](#tests)
-  * [Questions](#questions)
-
-
-  # Installation:
-  ${data.installation}
-
-
-  ## Usage:
-  ${data.usage}
-
-
-  ## License:
-  ${data.license}
-
-
-  ## Contributions:
-  ${data.contributions}
-
-
-  ## Tests:
-  ${data.tests}
-
-
-  ## Questions?
-  If you have any questions regarding the application, feel free to reach out to me directly at ${data.email}.
-  To view some of my other applications, check out my Github page at [${data.username}](https://github.com/${data.username}).
-`;
-}
+function licenseType(data) {
+  for (let i = 0; i < data.license.length; i++) {
+    const badgeType = data.license[i];
+    if (badgeType == "Apache"){
+      licenseBadge += "[![License: Apache v2](https://img.shields.io/badge/License-Apache%202.0-red.svg)](https://opensource.org/licenses/Apache-2.0)";
+  } if (badgeType == "MIT") {
+      licenseBadge += "[![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)";
+  } if (badgeType == "GNU") {
+      licenseBadge += "[![License: GNU v2](https://img.shields.io/badge/License-GNU%20v2-green.svg)](https://opensource.org/licenses/GPL-2.0)";
+  } if (badgeType == "BSD") {
+      licenseBadge += "[![License: BSD 3-Clause](https://img.shields.io/badge/License-BSD%203--Clause-yellow.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+  }
+  }
+  return licenseBadge;
+  }
+  
+  // This function generates the markdown for README and links the users input in to the ${""""""}.
+  function generateMarkdown(data) {
+    return `
+    # ${data.title}
+    ${licenseType(data)}
+    
+    ## Description:
+    ${data.description}
+    ## Table of contents:
+    * [License](#license)
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [Contributions](#contributions)
+    * [Tests](#tests)
+    * [Credits](#credits)
+    * [Author](#author)
+    * [Questions](#questions)
+    
+    ## License:
+    ${data.license}
+    
+    ## Installation:
+    ${data.installation}
+    
+    ## Usage:
+    ${data.usage}
+    
+    ## Contributions:
+    ${data.contributions}
+    
+    ## Tests: 
+    ${data.tests}
+    
+    ## Credits:
+    ${data.credits}
+    
+    ## Author:
+    ${data.author}
+    
+    
+    ## Questions:
+    For any questions regarding this application, you may reach me directly at ${data.email}. 
+    To view my other applications, please check out my github page [${data.username}](https://github.com/${data.username}).
+  `;
+  }
 
 module.exports = generateMarkdown;
